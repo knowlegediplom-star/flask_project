2820fa182bd0d37eb3988e0e8aed53d2ffd917cb
Светлана <sv.egorova6495@mail.ru>
2026-03-23 15:03:59 +0400

    задание 3 готово

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+notes = {}  # словарь для записей
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/notes", methods=["GET", "POST"])
+def notes_page():
+    if request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+
+        if title and text:
+            notes[title] = text
+
+        return redirect("/notes")
+
+    return render_template("notes.html", notes=notes)
 
 if __name__ == "__main__":
     app.run(debug=True)
