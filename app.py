609400aec2841763ac2a7b69a68792f46ba541af
Светлана <sv.egorova6495@mail.ru>
2026-03-23 15:54:06 +0400

    задание с базой данных выполнено

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
 
-notes = {}  # словарь для записей
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    subtitle = db.Column(db.String(100))
+    text = db.Column(db.Text)
 
 @app.route("/")
 def home():
@@ -12,13 +19,17 @@ def home():
 def notes_page():
     if request.method == "POST":
         title = request.form.get("title")
+        subtitle = request.form.get("subtitle")
         text = request.form.get("text")
 
         if title and text:
-            notes[title] = text
+            new_note = Note(title=title, subtitle=subtitle, text=text)
+            db.session.add(new_note)
+            db.session.commit()
 
         return redirect("/notes")
 
+    notes = Note.query.all()
     return render_template("notes.html", notes=notes)
 
 if __name__ == "__main__":
